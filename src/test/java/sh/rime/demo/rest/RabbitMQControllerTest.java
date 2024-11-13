30faf9979984b2279f3025dae67bd548a23e32dd
youta <youta@irain.io>
2024-11-14 01:08:45 +0800

    test: added unit tests

--- src/test/java/sh/rime/demo/rest/RabbitMQControllerTest.java
+++ src/test/java/sh/rime/demo/rest/RabbitMQControllerTest.java
@@ -34,12 +34,4 @@ class RabbitMQControllerTest {
         verify(rabbitMQSender).send(any(DemoMessage.class));
     }
 
-    @Test
-    void requestShouldFailWith401() {
-        webTestClient
-                .get().uri("/mq")
-                .exchange()
-                .expectStatus()
-                .isUnauthorized();
-    }
 }
